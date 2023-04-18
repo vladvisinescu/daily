7c55a660c9af61598405373d5160010c57351c9e
Vlad Visinescu <vladvisinescu@gmail.com>
2023-04-18 01:10:31 +0100

    Basic setup

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,9 +17,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->name();
+
+        $email = Str::of($name)->slug('.')->append('@example.net');
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $name,
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
